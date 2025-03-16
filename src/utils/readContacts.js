e3c80b33b185afcba6cc8d2758719842249865a6
Сергій Овчарук <ovcharuksvg@gmail.com>
2025-03-16 14:08:03 +0200

    fix path. Use path.join()

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,10 +1,11 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from '../contacts/contacts.js';
 import * as fs from 'node:fs/promises';
 
 export const readContacts = async () => {
   const data = await fs.readFile(PATH_DB, {
     encoding: 'utf-8',
   });
+  console.log(data);
   return JSON.parse(data);
 };
 
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,12 +1,6 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from '../contacts/contacts.js';
 import * as fs from 'node:fs/promises';
-import { readContacts } from './readContacts.js';
 
 export const writeContacts = async (users) => {
   await fs.writeFile(PATH_DB, JSON.stringify(users, null, 2));
 };
-// const newContacts = readContacts().then((cont) => {
-//   cont.push(...users);
-//  });
-
-// writeContacts().then((data) => console.log(data));
